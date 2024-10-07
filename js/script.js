9d5ad71de9b6161b9b669ef50cb4a1d123426bc3
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-07 21:34:27 +0600

    show video in UI

--- js/script.js
+++ js/script.js
@@ -65,22 +65,33 @@ const displayVideos = (videos) =>{
     const card = document.createElement('div');
     card.classList = 'card card-compact';
     card.innerHTML = `
-     <figure class="h-[200px]">
+     <figure class="h-[200px] relative">
     <img
       src=${video.thumbnail}
       alt="Shoes" class = "h-full w-full object-cover" />
+
+      ${video.others.posted_date?.length === 0 ? "" :`<span class = "absolute right-2 bottom-2 bg-black rounded p-1 text-white "> ${video.others.posted_date}</span>`}
+      
+
     </figure>
     <div class="px-0 py-2 flex gap-2">
     <div>
-    <img class = "w-10 h-10 rounded-full" src = "${video.authors[0].profile_picture} " />
+    <img class = "w-10 h-10 rounded-full" src = "${
+      video.authors[0].profile_picture
+    } " />
     </div>
     <div>
     <h2 class="font-bold"> ${video.title} </h2>
-    <div class= "flex items-center">
-    <P>${video.authors[0].profile_name}</P>
-    <img class = "w-5" src = '../images/icons8-verify-48.png'/>
+    <div class= "flex items-center gap-2">
+    <P class = "text-gray-400">${video.authors[0].profile_name}</P>
+    ${
+      video.authors[0].verified === true
+        ? "<img class = 'w-5' src = '../images/icons8-verify-48.png'/>"
+        : " "
+    }
 
     </div>
+    <P class = "text-gray-400">${video.others.views} Views</P>
     
     
     </div>
---
+++ test.js
@@ -0,0 +1,13 @@
+const isVarified = '' ;
+// if (isVarified) {
+//   console.log("user is varified")
+// }else{
+//   console.log("user is not varified")
+// }
+
+// console.log(`
+//   ${isVarified === true ? "User is varified" : "User is not varified"}`
+//     );
+
+const times = 16278 ;
+const days = 
